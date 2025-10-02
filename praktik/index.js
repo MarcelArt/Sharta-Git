dc0a6914862ab74280ebdd0974b93e3833bf8775
MarcelArt <badman.islemon@gmail.com>
2025-10-02 22:42:04 +0700

    merge conflict

--- praktik/index.js
+++ praktik/index.js
@@ -1,13 +1,8 @@
 const main = () => {
-<<<<<<< HEAD
     const a = 5;
     const b = 10;
     console.log("Hello from praktik!");
     console.log('a + b :>> ', a + b);
-=======
-    const hello = "hello";
-    console.log(`${hello} world`);
->>>>>>> a22dac1 (feat - hello world)
 }
 
 main();
