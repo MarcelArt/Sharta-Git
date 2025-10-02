ac88e9a73d9dae8fca6a7294a2fa25f96e4c9df4
MarcelArt <badman.islemon@gmail.com>
2025-10-02 23:16:01 +0700

    hello

--- praktik/index.js
+++ praktik/index.js
@@ -4,6 +4,11 @@ const main = () => {
     console.log(`${hello} ${world}`);
 
     console.log('Hello from praktik!');
+    const a = 5;
+    const b = 10;
+    console.log(a, b);
+    [b, a] = [a, b];
+    console.log(a, b, hello);
 }
 
 main();
