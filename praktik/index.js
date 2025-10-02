51caa0444305a273235b6796ad34d2a65536655d
MarcelArt <badman.islemon@gmail.com>
2025-10-02 22:28:49 +0700

    feat - hello world

--- praktik/index.js
+++ praktik/index.js
@@ -1,8 +1,13 @@
 const main = () => {
+<<<<<<< HEAD
     const a = 5;
     const b = 10;
     console.log("Hello from praktik!");
     console.log('a + b :>> ', a + b);
+=======
+    const hello = "hello";
+    console.log(`${hello} world`);
+>>>>>>> a22dac1 (feat - hello world)
 }
 
 main();
